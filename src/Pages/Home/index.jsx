15d05e212d29ecdd1b0c2c82a1e53246a9b7af4b
dangthang123 <dangthangtn123@gmail.com>
2023-03-15 08:06:09 +0700

    adđ

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,12 +8,24 @@ function Home(props) {
         <>
             <SliderHome />
             <div className='home-container max-w-[1320px] relative w-full px-[15px] m-auto '>
-                <div className='category-home'>
+                <div className='category-home px-[15px] mb-[120px]'>
                     {
                         [...new Array(3)].map((item, index) => (
                             <Category key={index} />
                         ))
                     }
+                </div>
+                <div className='logo-main w-full flex flex-wrap'>
+                    <p className='w-1/5'>aaa</p>
+                    <p className='w-1/5'>aaa</p>
+                    <p className='w-1/5'>aaa</p>
+                    <p className='w-1/5'>aaa</p>
+                    <p className='w-1/5'>aaa</p>
+                    <p className='w-1/5'>aaa</p>
+                    <p className='w-1/5'>aaa</p>
+                    <p className='w-1/5'>aaa</p>
+                    <p className='w-1/5'>aaa</p>
+                    <p className='w-1/5'>aaa</p>
                 </div>
             </div>
         </>

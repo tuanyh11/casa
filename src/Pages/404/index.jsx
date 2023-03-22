974a1865cd31b84003ee838142bab444ddd013ab
dangthang123 <dangthangtn123@gmail.com>
2023-03-22 16:22:03 +0700

    add

--- src/Pages/404/index.jsx
+++ src/Pages/404/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Breadcrumb } from '../../Components';
 import './style.css';
 function Page404(props) {
     return (
-        <div className=''>
+        <>
+            <Breadcrumb label={'404'} />
+            <div className='content-404'>
+                <div className='404-container max-w-[1320px] m-auto px-[15px] w-auto'>
+                    <div className='content-default-404'>
+                        <div className=''>
 
-        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
